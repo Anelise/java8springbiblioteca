9ec05a8d3e117da76ef87d327ea4e095222eb8a2
Anelise <ane.schunk@gmail.com>
2021-08-30 11:32:47 -0300

    iniciando projeto

---
+++ socialbooks/socialbooks/src/main/java/com/socialbooks/socialbooks/resources/LivrosResources.java
@@ -0,0 +1,14 @@
+package com.socialbooks.socialbooks.resources;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class LivrosResources {
+
+	@RequestMapping(value="livros", method= RequestMethod.GET )
+	public String listar() {
+		return " Casa Verda, azul";
+	}
+}
